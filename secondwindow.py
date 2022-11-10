cb48014ac5ce7995185b47415c669c9f09a4f981
마재성 <majesoung@naver.com>
2022-11-10 15:58:36 +0900

    리팩토링

--- secondwindow.py
+++ secondwindow.py
@@ -6,8 +6,6 @@ from PyQt5 import uic   # ui 파일을 사용하기 위한 모듈 import
 
 #UI파일 연결 코드
 
-
-
 form_secondwindow = uic.loadUiType("sub.ui")[0]
 
 class secondwindow(QMainWindow, form_secondwindow) :
@@ -16,8 +14,11 @@ class secondwindow(QMainWindow, form_secondwindow) :
         super().__init__()
         self.state=state
         self.qnum=qnum
-        self.totalcount=totalcount
+        self.totalCase=totalcount
         self.currentpage=1
+        self.pixmap=QPixmap("퀸블랙.png")
+        self.font=QFont('NanumGothic',16)
+        self.font.setBold(True)
         self.setFixedSize(QSize(1000, 750))
         self.setupUi(self)
         self.setWindowIcon(QIcon('퀸블랙.png'))
@@ -30,12 +31,9 @@ class secondwindow(QMainWindow, form_secondwindow) :
         
         
     def init_gridlayout(self):
-        pixmap=QPixmap("퀸블랙.png")
-        pixmap1=pixmap.scaled(450/self.qnum,450/self.qnum,Qt.KeepAspectRatio)
-        
+                
         widget=QWidget(self)
         
-        queeninfo=self.state
         
         gridLayout=self.chessLayout
         gridLayout.setSpacing(0)
@@ -45,24 +43,23 @@ class secondwindow(QMainWindow, form_secondwindow) :
         numberColLay=self.numberColLayout
         
         for i in range(self.qnum):
-            font=QFont('NanumGothic',16)
-            font.setBold(True)
+
             text=QLabel()
             text1=QLabel()
-            text.setFont(font)
-            text1.setFont(font)
+            text.setFont(self.font)
+            text1.setFont(self.font)
             text.setText(f"{i}")
             text1.setText(f"{i}")
             text.setAlignment(Qt.AlignCenter)
-            text1.setAlignment(Qt.AlignCenter)
-            
+            text1.setAlignment(Qt.AlignCenter)            
             numberRowLay.addWidget(text,i,0)
-            
             numberColLay.addWidget(text1,0,i)
+
             for j in range(self.qnum):
+
                 board=QLabel()
-               
                 board.setAlignment(Qt.AlignCenter)
+
                 if(i%2!=0):
                     
                     if(j%2==0):
@@ -74,7 +71,6 @@ class secondwindow(QMainWindow, form_secondwindow) :
                         
                         board.setStyleSheet("background-color:white;"
                                             "border:1px solid black;")
-
                         gridLayout.addWidget(board,i,j,1,1)
                 else:
                     if(j%2==0):
@@ -89,14 +85,13 @@ class secondwindow(QMainWindow, form_secondwindow) :
         self.setCentralWidget(widget)
     
     def init_button(self):
-        font=QFont('NanumGothic',16)
-        font.setBold(True)
+        
         
         RButton=QPushButton(self)
         LButton=QPushButton(self)
 
-        RButton.setFont(font)
-        LButton.setFont(font)
+        RButton.setFont(self.font)
+        LButton.setFont(self.font)
         
         RButton.setText('>')
         LButton.setText('<')
@@ -104,15 +99,15 @@ class secondwindow(QMainWindow, form_secondwindow) :
         LButton.setGeometry(300,675,81,41)
         RButton.setGeometry(590,675,81,41)
         
-        RButton.clicked.connect(self.btn_click1)
-        LButton.clicked.connect(self.btn_click2)
+        RButton.clicked.connect(self.btn_RClick)
+        LButton.clicked.connect(self.btn_LClick)
         
     def show_page(self,current:int):
-        self.page.setText(f'{current} / {self.totalcount}')
+        self.page.setText(f'{current} / {self.totalCase}')
             
         
-    def btn_click1(self):
-        if(self.currentpage==self.totalcount):
+    def btn_RClick(self):
+        if(self.currentpage==self.totalCase):
             QMessageBox.information(self,"알림","마지막 페이지입니다")
             return False
         else:
@@ -123,7 +118,7 @@ class secondwindow(QMainWindow, form_secondwindow) :
             self.show_page(self.currentpage)
         
         
-    def btn_click2(self):
+    def btn_LClick(self):
         if(self.currentpage==1):
             QMessageBox.information(self,"알림","마지막 페이지입니다")
             return False
@@ -136,8 +131,7 @@ class secondwindow(QMainWindow, form_secondwindow) :
             
         
     def setQueen(self,state,current):
-        pixmap=QPixmap("퀸블랙.png")
-        pixmap1=pixmap.scaled(450/self.qnum,450/self.qnum,Qt.KeepAspectRatio)
+        pixmap1=self.pixmap.scaled(450/self.qnum,450/self.qnum,Qt.KeepAspectRatio)
         qInfo=state[current]
         chessBoard=self.chessLayout
         for i in range(self.qnum):
@@ -153,13 +147,9 @@ class secondwindow(QMainWindow, form_secondwindow) :
             queenPos=chessBoard.itemAtPosition(i,qInfo[i]).widget()
             queenPos.setPixmap(pixmap)
 
-    
-        
-        
-        
 
 if __name__ == "__main__" :
     app = QApplication(sys.argv) 
     Window = secondwindow() 
-    Window.show()
+    Window.show()   
     app.exec_()
